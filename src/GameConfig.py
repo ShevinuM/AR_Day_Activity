5d65bbd011a23593d55753fbeb1c1653c609d677
ShevinuM <nsmcooray@mun.ca>
2023-09-12 18:05:20 -0230

    Update

--- src/GameConfig.py
+++ src/GameConfig.py
@@ -10,11 +10,11 @@ FPS_LIMIT = 2
 END_SCREEN_WAIT_TIME_SEC = 3
 
 GAME_LEVELS = [
-    GameLevel(name='Level 1, no AI', num_ai_drives=0, num_pods=10, is_target_pod_required=False, sensor_range=-1),
-    GameLevel(name='Level 2, 1 AI', num_ai_drives=1, num_pods=20, is_target_pod_required=False, sensor_range=-1),
-    GameLevel(name='Level 3, 5 AI', num_ai_drives=5, num_pods=30, is_target_pod_required=False, sensor_range=-1),
-    GameLevel(name='Level 4, 20 AI', num_ai_drives=20, num_pods=30, is_target_pod_required=False, sensor_range=-1),
-    GameLevel(name='Level 5, Advanced - 5 AI', num_ai_drives=5, num_pods=10, is_target_pod_required=True, sensor_range=-1)
+    GameLevel(name='Level 1, no AI', num_ai_drives=0, num_pods=10, is_target_pod_required=False, sensor_range=-1)
+    #GameLevel(name='Level 2, 1 AI', num_ai_drives=1, num_pods=20, is_target_pod_required=False, sensor_range=-1),
+    #GameLevel(name='Level 3, 5 AI', num_ai_drives=5, num_pods=30, is_target_pod_required=False, sensor_range=-1),
+    #GameLevel(name='Level 4, 20 AI', num_ai_drives=20, num_pods=30, is_target_pod_required=False, sensor_range=-1),
+    #GameLevel(name='Level 5, Advanced - 5 AI', num_ai_drives=5, num_pods=10, is_target_pod_required=True, sensor_range=-1)
 ]
 
 MAX_MOVES_PER_ROUND = 1000
